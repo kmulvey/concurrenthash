0108a394a01027a44f29446da3c6a67d3e7e359a
Kevin Mulvey <kmulvey@linux.com>
2022-05-16 18:23:46 -0600

    cli tool

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"runtime"
 
 	"github.com/kmulvey/concurrenthash"
 	"github.com/twmb/murmur3"
 )
 
 func main() {
+	var file string
+	var threads int
+	var blockSize int
+
+	flag.StringVar(&file, "file", "", "file to hash (abs path)")
+	flag.IntVar(&threads, "threads", 1, "number of threads to use, >1 only useful when rebuilding the cache")
+	flag.IntVar(&blockSize, "block-size", 1e6, "size of chunk to hash")
+	flag.Parse()
+
+	if threads <= 0 || threads > runtime.GOMAXPROCS(0) {
+		threads = 1
+	}
+
 	var ch = concurrenthash.NewConcurrentHash(2, 2, murmur3.New64())
-	fmt.Println(ch.HashFile("1g.img"))
+	var hash, err = ch.HashFile("1g.img")
+	if err != nil {
+		fmt.Printf("Encountered an error: %s", err.Error())
+		return
+	}
+
+	fmt.Printf("%s: %s\n", file, hash)
 }
