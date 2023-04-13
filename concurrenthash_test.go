54f29560ce151ef148ecebc7d0bd5c522a39669e
Kevin Mulvey <kmulvey@linux.com>
2023-04-13 09:40:18 -0600

    weak tests for windows

--- concurrenthash_test.go
+++ concurrenthash_test.go
@@ -19,6 +19,6 @@ func TestEverything(t *testing.T) {
 	assert.Equal(t, "bf842e96b246556052bc7e518de1fdf7c4a5a859ad104a201880074bece30b82", sum)
 
 	sum, err = cs.HashFile(ctx, "./sdfsdfsf.txt")
-	assert.Contains(t, err.Error(), "stat ./sdfsdfsf.txt:")
+	assert.Contains(t, err.Error(), "./sdfsdfsf.txt:") // this is quite loose because windows
 	assert.Equal(t, "", sum)
 }
