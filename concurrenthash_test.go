5c8f5bae65f54271e733abc3d864481289cc2107
Kevin Mulvey <kmulvey@linux.com>
2024-02-22 10:05:19 -0700

    fix test data

--- concurrenthash_test.go
+++ concurrenthash_test.go
@@ -16,7 +16,7 @@ func TestEverything(t *testing.T) {
 	var cs = NewConcurrentHash(2, 10, sha256.New)
 	var sum, err = cs.HashFile(ctx, "./rand-file.txt")
 	assert.NoError(t, err)
-	assert.Equal(t, "bf842e96b246556052bc7e518de1fdf7c4a5a859ad104a201880074bece30b82", sum)
+	assert.Equal(t, "4870bc3d9a751543ee66685fde1a81a78265bed8e532878fb45da0cb08aa5f3c", sum)
 
 	sum, err = cs.HashFile(ctx, "./sdfsdfsf.txt")
 	assert.Contains(t, err.Error(), "./sdfsdfsf.txt:") // this is quite loose because windows
